7c06fea9e98137670f45427bf54fed801f132c6b
mdunggggg <dungmanh74@gmail.com>
2023-08-18 21:44:36 +0700

    Created

---
+++ J01001.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.javaptit;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author dungm
+ */
+public class J01001 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        int y = scanner.nextInt();
+        if(x <= 0 || y <= 0){
+            System.out.println("0");
+        }
+        else{
+            System.out.println((x + y) *2 + " " + x * y);
+        }
+    }
+}
